0128e2b7834461b21a5049f35c1b2baef14f22d8
mokchenda <mokchend@yahoo.com>
2018-09-30 15:57:10 +0200

    Adding lint formatter rules

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * index.js
  */
 const logSomething = options => ({
-    ...options,
-    anotherOption: 'Hello!'
+  ...options,
+  anotherOption: 'Hello!',
 });
 const options = logSomething({ one: '1', two: '2' });
 console.log(options);
